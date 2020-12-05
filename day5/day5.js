4258bd1d02b68d5f4bcd75edcb76cc4677b324e1
3d12 <3d12@users.noreply.github.com>
2020-12-05 12:32:46 -0600

    Adding Day 5

---
+++ day5/day5.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const readline = require('readline');
+
+let passArray = []
+
+async function openFileForReading(file) {
+	const fileStream = fs.createReadStream(file);
+
+	const rl = readline.createInterface({
+		input: fileStream,
+		crlfDelay: Infinity
+	});
+
+	for await (const line of rl) {
+		passArray.push(line);
+	}
+}
+
+class BinaryDirectionError extends Error {}
+
+function binarySplit(direction, begin, end) {
+	let total = (end+1) - begin;
+	let midpoint = begin + (total / 2) - 1;
+	if (direction == "F" || direction == "L") {
+		return [ begin, midpoint ];
+	}
+	if (direction == "B" || direction == "R") {
+		return [ midpoint+1, end ];
+	}
+	return new BinaryDirectionError("Invalid direction: " + direction);
+}
+
+function calculateSeatID(seat) {
+	return (seat.row * 8) + seat.col;
+}
+
+function parseBoardingPass(line) {
+	let minRow = 0;
+	let maxRow = 127;
+	let minCol = 0;
+	let maxCol = 7;
+	let readIndex = 0;
+	for (const direction of line) {
+		if (readIndex < 7) {
+			[minRow, maxRow] = binarySplit(direction, minRow, maxRow);
+		}
+		if (readIndex >= 7) {
+			[minCol, maxCol] = binarySplit(direction, minCol, maxCol);
+		}
+		readIndex++;
+	}
+	return { row: minRow, col: minCol };
+}
+
+(async function mainExecution() {
+	await openFileForReading('input.txt');
+	let idMap = passArray.map((pass) => {
+		return calculateSeatID(parseBoardingPass(pass));
+	});
+	console.log("The highest seat ID is: " + idMap.reduce((a,b) => {
+		return Math.max(a,b);
+	}));
+})();
+
